57d5c19e1fd9fce05caaf645e086334e8ffe430a
Fyleau <geekrato.gk@gmail.com>
2026-01-07 15:39:05 +0100

    v16

--- radius/init_db.sql
+++ radius/init_db.sql
@@ -1,1 +1,43 @@
--- init_db.sql for PostgreSQL
+-- init_db.sql for PostgreSQL with FreeRADIUS schema
+
+CREATE TABLE radcheck (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL DEFAULT '',
+  attribute TEXT NOT NULL DEFAULT '',
+  op VARCHAR(2) NOT NULL DEFAULT '==',
+  value TEXT NOT NULL DEFAULT ''
+);
+
+CREATE TABLE radreply (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL DEFAULT '',
+  attribute TEXT NOT NULL DEFAULT '',
+  op VARCHAR(2) NOT NULL DEFAULT '=',
+  value TEXT NOT NULL DEFAULT ''
+);
+
+CREATE TABLE radusergroup (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL DEFAULT '',
+  groupname TEXT NOT NULL DEFAULT '',
+  priority INTEGER NOT NULL DEFAULT 1
+);
+
+CREATE TABLE radgroupcheck (
+  id SERIAL PRIMARY KEY,
+  groupname TEXT NOT NULL DEFAULT '',
+  attribute TEXT NOT NULL DEFAULT '',
+  op VARCHAR(2) NOT NULL DEFAULT '==',
+  value TEXT NOT NULL DEFAULT ''
+);
+
+CREATE TABLE radgroupreply (
+  id SERIAL PRIMARY KEY,
+  groupname TEXT NOT NULL DEFAULT '',
+  attribute TEXT NOT NULL DEFAULT '',
+  op VARCHAR(2) NOT NULL DEFAULT '=',
+  value TEXT NOT NULL DEFAULT ''
+);
+
+-- Test User
+INSERT INTO radcheck (username, attribute, op, value) VALUES ('testuser', 'Cleartext-Password', ':=', 'testpass');
